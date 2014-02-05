42ab6292c634a936e3c353deb1de48acdb1fd377
Timur Kalimov <tkalimov@gmail.com>
2014-02-05 11:26:47 -0500

    api only middleware

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module AlphaApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.api_only = false
+    
+    # Include full middleware
+    # config.api_only = false
   end
 end
