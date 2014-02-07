0bfa7660e5fe04f7a752ac0d9ea5aee8ba477f96
Timur Kalimov <tkalimov@gmail.com>
2014-02-07 11:54:56 -0500

    change authentication to header instead of param

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -23,7 +23,7 @@ module Api
       end 
 
       def authenticate_user_from_token!
-	    user_token = params[:user_token].presence
+	    user_token = request.headers['user-token']
 	    user       = user_token && User.find_by_authentication_token(user_token.to_s)
 	 
 	    if user
