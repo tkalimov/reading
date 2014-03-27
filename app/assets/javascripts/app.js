59fb9e5b77d839366aadcc84855d76797ba6e6d5
Joe Kerns <jmkerns@gmail.com>
2014-03-27 15:32:47 -0400

    pre-merge with tim

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,6 @@
 var AlphaApi = angular.module('AlphaApi', ['ngResource', 'ui.bootstrap', 'ngRoute']);
 
+// ROUTES FOR PARTIALS
 
 AlphaApi.config(function($routeProvider) {
 $routeProvider
@@ -22,6 +23,27 @@ $routeProvider
       controller: 'SignInCtrl'
     })
 })
+
+// CONTROLLERS
+
+.controller('SignInCtrl', function($scope, $http, $location) {
+  console.log('SignInCtrl');
+  $scope.signInUser = function(user) {
+    $http.post('http://localhost:3000/api/v1/users/sign_in', {user:user}).
+      success(function(response) {
+        console.log('login successful');
+        window.localStorage.setItem("auth_token", response.auth_token);
+        $location.path('/home');
+      }).
+      error(function(response) {
+        $scope.errors = response.message;
+        console.log('error');
+    });
+  };
+})
+
+// OTHER JAVASCRIPT ACTIVITIES
+
 // TEST PHRASING TO CHECK THAT ANGULAR WORKS
 function AngularRocksCtrl($scope) {
 	$scope.message = "Angular Rocks!"
