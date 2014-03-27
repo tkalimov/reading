0a0d72156f7deb90681dd86eb53f6590f87c2f19
Timur Kalimov <tkalimov@gmail.com>
2014-03-27 17:28:58 -0400

    tweaking for heroku

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,13 +4,12 @@ module Api
 		      after_filter :cors_set_access_control_headers
 
 		      def create
-		        user = User.new(user_params)
-
-		        if user.save
-		          render :json=> {:success=>true, :auth_token=>user.authentication_token, :email=>user.email}
-		          sign_in user
+		        @user = User.new(user_params)
+		        if @user.save
+		          render :json=> {:success=>true, :auth_token=>@user.authentication_token, :email=>@user.email}
+		          sign_in @user
 		        else
-		          render json: user.errors, status: :unprocessable_entity
+		          render json: @user.errors, status: :unprocessable_entity
 		        end
 		      end
 		      
