5201015f77ac5bc6b51d047472e9c72bf724edfc
Timur Kalimov <tkalimov@gmail.com>
2014-03-27 17:32:24 -0400

    more heroku tweaks

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -16,7 +16,7 @@ module Api
 		      private 
 		      
 		      def user_params
-		          params.require(:user).permit(:first_name, :last_name, :email, :password, :business_name, :avatar)
+		          params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
 		      end 
 
 		end 
